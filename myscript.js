19ab0f45538c3be34f7f70d992bfdb1bd35578d7
secseminarfunq2re <64069506+secseminarfunq2re@users.noreply.github.com>
2020-04-21 10:55:33 +0200

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,2 +1,7 @@
-document.body.style.backgroundColor = "red";
-document.getElementsByClassName("container")[0].getElementsByTagName('h3')[0].innerText = "pwned";
+function ready() {
+  document.body.style.backgroundColor = "red";
+  document.getElementsByClassName("container")[0].getElementsByTagName('h3')[0].innerText = "pwned";
+}
+
+document.addEventListener("DOMContentLoaded", ready);
+
